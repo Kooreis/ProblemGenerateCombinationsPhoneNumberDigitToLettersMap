82cea2e13a5a66d2f313bc7d57c160e1bd94f0fe
Kooreis <kooreis.official@gmail.com>
2024-07-01 10:01:22 -0500

    This update defines a function that generates all combinations of a phone number. It creates a generator that yields the letters corresponding to each digit in the phone number, then uses the product function from the itertools module to generate all combinations of these letters.

--- solution.py
+++ solution.py
@@ -1,12 +1,3 @@
-from itertools import product
-
-digit_to_letters = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz',
-}
+def phone_number_combinations(phone_number):
+    letters_to_combine = (digit_to_letters[digit] for digit in phone_number if digit in digit_to_letters)
+    return [''.join(combination) for combination in product(*letters_to_combine)]
