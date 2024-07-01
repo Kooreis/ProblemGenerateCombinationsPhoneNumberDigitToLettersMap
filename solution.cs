a54c9f2c5202d4fe63e93e26644336d2c94dc455
Kooreis <kooreis.official@gmail.com>
2024-07-01 10:01:22 -0500

    The second part of the script defines a function that takes a string of digits as input and initializes a list to store the results. It also sets up a mapping from digits to letters.

--- solution.cs
+++ solution.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main(string[] args)
+public static IList<string> LetterCombinations(string digits)
     {
-        Console.Write("Enter a phone number: ");
-        string digits = Console.ReadLine();
+        IList<string> res = new List<string>();
+        if (string.IsNullOrEmpty(digits)) return res;
+        string[] mapping = new string[10]
+        {
+            "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        res.Add("");
     }
-}
